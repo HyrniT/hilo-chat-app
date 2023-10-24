096787e3c5c9c492cbe7829675b125b997b13024
20120077 <ngqghien@gmail.com>
2023-10-25 00:47:07 +0700

    Authenticate with OTP (have bugs)
    Bugs: Cannot receive the code from the Firebase via correct phone number

--- app/src/main/java/com/example/hilo/LoginOtpActivity.java
+++ app/src/main/java/com/example/hilo/LoginOtpActivity.java
@@ -1,19 +1,159 @@
 package com.example.hilo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.hilo.utils.AndroidUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthMissingActivityForRecaptchaException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginOtpActivity extends AppCompatActivity {
     private String phoneNumber;
+    private EditText txtLoginOtp;
+    private Button btnNext;
+    private ProgressBar pgbLogin;
+    private TextView txtResend;
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private Long timeoutSeconds = 60L;
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_otp);
 
+        txtLoginOtp = findViewById(R.id.edittext_login_otp);
+        btnNext = findViewById(R.id.button_login_next);
+        pgbLogin = findViewById(R.id.progress_bar_login);
+        txtResend = findViewById(R.id.text_view_resend);
+
         phoneNumber = getIntent().getExtras().getString("phone");
-//        Toast.makeText(getApplicationContext(), phoneNumber, Toast.LENGTH_LONG).show();
+        AndroidUtil.showToast(getApplicationContext(), phoneNumber);
+
+        sendOtp(phoneNumber, false);
+    }
+
+    private void setInProgress(boolean inProgress) {
+        if (inProgress) {
+            pgbLogin.setVisibility(View.VISIBLE);
+            btnNext.setVisibility(View.GONE);
+        } else {
+            pgbLogin.setVisibility(View.GONE);
+            btnNext.setVisibility(View.VISIBLE);
+        }
+    }
+
+    // https://firebase.google.com/docs/auth/android/phone-auth
+    private void sendOtp(String phoneNumber, boolean isResend) {
+        setInProgress(true);
+
+        PhoneAuthOptions.Builder builder =
+                PhoneAuthOptions.newBuilder()
+                        .setPhoneNumber(phoneNumber)       // Phone number to verify
+                        .setTimeout(timeoutSeconds, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // (optional) Activity for callback binding
+                        // If no activity is passed, reCAPTCHA verification can not be used.
+                        .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                // This callback will be invoked in two situations:
+                                // 1 - Instant verification. In some cases the phone number can be instantly
+                                //     verified without needing to send or enter a verification code.
+                                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                                //     detect the incoming verification SMS and perform verification without
+                                //     user action.
+
+                                // Log.d(TAG, "onVerificationCompleted:" + phoneAuthCredential);
+
+                                signInWithPhoneAuthCredential(phoneAuthCredential);
+                                setInProgress(false);
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                // This callback is invoked in an invalid request for verification is made,
+                                // for instance if the the phone number format is not valid.
+                                //  Log.w(TAG, "onVerificationFailed", e);
+
+                                if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                                    // Invalid request
+                                } else if (e instanceof FirebaseTooManyRequestsException) {
+                                    // The SMS quota for the project has been exceeded
+                                } else if (e instanceof FirebaseAuthMissingActivityForRecaptchaException) {
+                                    // reCAPTCHA verification attempted with null Activity
+                                }
+
+                                AndroidUtil.showToast(getApplicationContext(), "OTP verification failed");
+                                setInProgress(false);
+                            }
+
+                            @Override
+                            public void onCodeSent(@NonNull String verificationId,
+                                                   @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                                // The SMS verification code has been sent to the provided phone number, we
+                                // now need to ask the user to enter the code and then construct a credential
+                                // by combining the code with a verification ID.
+                                // Log.d(TAG, "onCodeSent:" + verificationId);
+                                super.onCodeSent(verificationId, token);
+
+                                // Save verification ID and resending token so we can use them later
+                                mVerificationId = verificationId;
+                                mResendToken = token;
+
+                                AndroidUtil.showToast(getApplicationContext(), "OTP sent successfully");
+                                setInProgress(false);
+                            }
+                        });          // OnVerificationStateChangedCallbacks
+
+        if (isResend) {
+            PhoneAuthProvider.verifyPhoneNumber(builder.setForceResendingToken(mResendToken).build());
+        } else {
+            PhoneAuthProvider.verifyPhoneNumber(builder.build());
+        }
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            // Log.d(TAG, "signInWithCredential:success");
+
+                            FirebaseUser user = task.getResult().getUser();
+                            // Update UI
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            // Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                            }
+                        }
+                    }
+                });
     }
 }
