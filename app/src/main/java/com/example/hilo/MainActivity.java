993a735463d9635a6b7f14fe6f0ac94bd85d9561
20120077 <ngqghien@gmail.com>
2023-10-28 01:03:32 +0700

    Add chat and profile fragments in MainActivity

--- app/src/main/java/com/example/hilo/MainActivity.java
+++ app/src/main/java/com/example/hilo/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.hilo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private ImageButton btnSearch;
+    private Fragment chatFragment, profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        btnSearch = findViewById(R.id.btnSearch);
+
+        chatFragment = new ChatFragment();
+        profileFragment = new ProfileFragment();
+
+        bottomNavigationView.setSelectedItemId(R.id.menu_chat);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.menu_chat) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, chatFragment).commit();
+                }
+                if (item.getItemId() == R.id.menu_profile) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, profileFragment).commit();
+                }
+                return true;
+            }
+        });
     }
 }
