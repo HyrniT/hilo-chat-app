028d7c8d05d6fd97d0a59af28ee774b062a66e11
20120077 <ngqghien@gmail.com>
2023-11-03 01:15:02 +0700

    Add notification from device to device

--- app/src/main/java/com/example/hilo/ChatActivity.java
+++ app/src/main/java/com/example/hilo/ChatActivity.java
@@ -28,7 +28,18 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.Query;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.Arrays;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -125,7 +136,61 @@ public class ChatActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<DocumentReference> task) {
                 if (task.isSuccessful()) {
                     txtChat.setText("");
+                    sendNotification(message);
                 }
+            }
+        });
+    }
+
+    private void sendNotification(String message) {
+        FirebaseUtil.getCurrentUserReference().get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    UserModel currentUser = task.getResult().toObject(UserModel.class);
+                    try {
+                        JSONObject jsonObject = new JSONObject();
+
+                        JSONObject notificationObject = new JSONObject();
+                        notificationObject.put("title", currentUser.getUsername());
+                        notificationObject.put("body", message);
+
+                        JSONObject dataObject = new JSONObject();
+                        dataObject.put("userId", currentUser.getUserId());
+
+                        jsonObject.put("notification", notificationObject);
+                        jsonObject.put("data", dataObject);
+                        jsonObject.put("to", otherUserModel.getFCMToken());
+
+                        callApi(jsonObject);
+                    } catch (Exception e) {
+
+                    }
+                }
+            }
+        });
+    }
+
+    void callApi(JSONObject jsonObject) {
+        MediaType JSON = MediaType.get("application/json");
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "https://fcm.googleapis.com/fcm/send";
+        RequestBody body = RequestBody.create(jsonObject.toString(), JSON);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .header("Authorization", "Bearer AAAAckVxD5A:APA91bGVFVJNC-l9GsI_vrIeE6XmhT7sAsCbckxyBHjNYT2J48aXJrKfIDlnib4KLl5R7_PS5Qzwj5dAuxetdiSfRA5y036jHSa6MbX8W-GFcxJ0i9W23oOp9_1zXAigKkaKoWQoLulG")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+
             }
         });
     }
