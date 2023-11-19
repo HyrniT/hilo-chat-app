7353b4191f59a4055fc04275448f9cded07ae152
20120077 <ngqghien@gmail.com>
2023-11-19 09:32:19 +0700

    Refactor ViewPagerChatAdapter

--- app/src/main/java/com/example/hilo/ChatFragment.java
+++ app/src/main/java/com/example/hilo/ChatFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.hilo.adapter.ViewPagerChatAdapter;
 import com.google.android.material.tabs.TabLayout;
 
 public class ChatFragment extends Fragment {
--- app/src/main/java/com/example/hilo/adapter/ViewPagerChatAdapter.java
+++ app/src/main/java/com/example/hilo/adapter/ViewPagerChatAdapter.java
@@ -1,9 +1,12 @@
-package com.example.hilo;
+package com.example.hilo.adapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.hilo.ContactFragment;
+import com.example.hilo.GroupFragment;
 
 public class ViewPagerChatAdapter extends FragmentStateAdapter {
     public ViewPagerChatAdapter(@NonNull FragmentActivity fragmentActivity) {
