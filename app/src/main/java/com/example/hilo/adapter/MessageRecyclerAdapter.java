69c9d9961e9f3c1f3b3e5c33219eb6acce9e8f39
20120077 <ngqghien@gmail.com>
2023-11-20 23:36:50 +0700

    Update popup menu layout and add to chat activity

--- app/src/main/java/com/example/hilo/adapter/MessageRecyclerAdapter.java
+++ app/src/main/java/com/example/hilo/adapter/MessageRecyclerAdapter.java
@@ -1,12 +1,21 @@
 package com.example.hilo.adapter;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -79,6 +88,95 @@ public class MessageRecyclerAdapter extends FirestoreRecyclerAdapter<MessageMode
             txtRightMessage = itemView.findViewById(R.id.txtRightMessage);
             imgLeftMessage = itemView.findViewById(R.id.imgLeftMessage);
             imgRightMessage = itemView.findViewById(R.id.imgRightMessage);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getBindingAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        MessageModel selectedMessage = getItem(position);
+                        if (selectedMessage != null) {
+//                            AndroidUtil.showToast(context, String.valueOf(position));
+                        }
+                    }
+                }
+            });
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    int position = getBindingAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        MessageModel selectedMessage = getItem(position);
+                        if (selectedMessage != null) {
+                            showPopupMenu(view, selectedMessage);
+                        }
+                    }
+                    return true;
+                }
+            });
+        }
+
+        private void showPopupMenu(View anchorView, MessageModel message) {
+            PopupMenu popupMenu = new PopupMenu(itemView.getContext(), anchorView);
+            popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    // Handle menu item click here
+                    if (item.getItemId() == R.id.menu_pin) {
+                        // Handle pin action
+                    } else if (item.getItemId() == R.id.menu_delete) {
+                        // Handle delete action
+                    } else if (item.getItemId() == R.id.menu_copy) {
+                        // Handle copy action
+                    } else if (item.getItemId() == R.id.menu_reply) {
+                        // Handle reply action
+                    } else if (item.getItemId() == R.id.menu_forward) {
+                        // Handle forward action
+                    }
+
+                    return true;
+                }
+            });
+
+            // Add icon for menu item
+            Menu menu = popupMenu.getMenu();
+            for (int i = 0; i < menu.size(); i++) {
+                MenuItem menuItem = menu.getItem(i);
+                SpannableString spannable = new SpannableString("    " + menuItem.getTitle());
+                Drawable drawable = menuItem.getIcon();
+
+                if (drawable != null) {
+                    drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+                    ImageSpan imageSpan = new ImageSpan(drawable, ImageSpan.ALIGN_CENTER);
+                    spannable.setSpan(imageSpan, 0, 1, Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+                }
+
+                menuItem.setTitle(spannable);
+            }
+
+
+            // Determine the gravity and position for the popup menu
+            int[] location = new int[2];
+            anchorView.getLocationOnScreen(location);
+            int anchorViewY = location[1];
+
+            int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+            int halfScreenHeight = screenHeight / 2;
+
+            if (message.getSenderId().equals(FirebaseUtil.getCurrentUserId())) {
+                popupMenu.setGravity(Gravity.END);
+            } else {
+                if (anchorViewY > halfScreenHeight) {
+                    popupMenu.setGravity(Gravity.TOP);
+                } else {
+                    popupMenu.setGravity(Gravity.BOTTOM);
+                }
+            }
+
+            popupMenu.show();
         }
     }
 }
