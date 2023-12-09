40f184097bb93dd6c637ff7386db91dae8de5f64
20120077 <ngqghien@gmail.com>
2023-12-09 20:48:50 +0700

    Fix bugs: Encrypt image in photo view

--- app/src/main/java/com/example/hilo/PhotoViewActivity.java
+++ app/src/main/java/com/example/hilo/PhotoViewActivity.java
@@ -2,11 +2,14 @@ package com.example.hilo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
 import com.bumptech.glide.Glide;
+import com.example.hilo.utils.AndroidUtil;
+import com.example.hilo.utils.EncryptionUtil;
 import com.github.chrisbanes.photoview.PhotoView;
 
 public class PhotoViewActivity extends AppCompatActivity {
@@ -23,9 +26,18 @@ public class PhotoViewActivity extends AppCompatActivity {
         photoView = findViewById(R.id.photoView);
         btnClose = findViewById(R.id.btnClose);
 
-        Glide.with(this)
-                .load(imageUrl)
-                .into(photoView);
+        try {
+            byte[] decryptedImageUrlBytes = EncryptionUtil.decryptImage(imageUrl);
+            if (decryptedImageUrlBytes != null) {
+                String decryptedImageUrl = new String(decryptedImageUrlBytes);
+
+                Glide.with(this)
+                        .load(decryptedImageUrl)
+                        .into(photoView);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         btnClose.setOnClickListener(new View.OnClickListener() {
             @Override
